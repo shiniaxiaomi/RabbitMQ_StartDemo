7a79c4ae8791c88102f8076d369a3390e2877d43
shiniaxiaomi <lyj.8066@qqcom>
2019-11-24 23:44:03 +0800

    init

---
+++ src/main/java/two/DistributeMessages/NewTask.java
@@ -0,0 +1,46 @@
+package two.DistributeMessages;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//发送多条消息
+public class NewTask {
+
+    private final static String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws Exception {
+
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+
+            while (true) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String s = reader.readLine();
+
+                channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+                String message = String.join(" ", s);
+
+                channel.basicPublish("", TASK_QUEUE_NAME,
+                        MessageProperties.PERSISTENT_TEXT_PLAIN,
+                        message.getBytes("UTF-8"));
+                System.out.println(" [x] Sent '" + message + "'");
+
+            }
+
+        }
+
+    }
+
+
+}
---
+++ src/main/java/two/DistributeMessages/Worker.java
@@ -0,0 +1,33 @@
+package two.DistributeMessages;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+//启动多个worker来消费消息
+public class Worker {
+
+    private final static String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        channel.basicQos(1);//设置一次只能接受一个消息，因为如果消息消费成功后，将成功的信息返回RabbitMQ必须使用同一个Channel，不然会抛出异常
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        boolean autoAck = true; // 设置如果消费消息成功将会返回信息给RabbitMQ，这样做的目的是防止数据丢失，RabbitMQ接收到返回的信息才会将消息删除掉
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+
+    }
+
+}
